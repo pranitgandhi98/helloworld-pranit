7de493357a8c9ab9739156478746173e32eb105b
pranit gandhi <gmail@gmail.com>
2020-09-08 08:58:49 +0000

    commit from newfeature branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!, This is an edit from feature branch."); 
+        System.out.println("Hello, World!, This is an edit from "newfeature" branch."); 
     }
 }
