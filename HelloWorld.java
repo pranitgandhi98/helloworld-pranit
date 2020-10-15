809a160efc2dfe1d67d3037e82ef7a2051bba1c3
pranitgandhi98 <pranitgandhi98@gmail.com>
2020-10-15 09:05:52 +0530

    Changes to HelloWorld.java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
 	int i;
-	for(i = 0; i <= 5; i++) {
+	for(i = 0; i <= 10; i++) {
 		System.out.println("\n Hello World ");
 	}
     }
